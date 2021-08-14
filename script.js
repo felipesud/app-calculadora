680da2c1b4f6571ddcf77a2d868c212e552b33cf
Felipe Belisário <felipesantos.dsb@hotmail.com>
2021-08-13 21:41:51 -0300

    update script

---
+++ script.js
@@ -0,0 +1,19 @@
+function calcular(tipo, valor){
+   if (tipo === 'acao'){
+    if(valor === 'c'){
+        //limpar o visor (id resultado)
+        document.getElementById('resultado').value = ' '
+    }   
+
+    if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.'){
+        document.getElementById('resultado').value += valor
+    }
+
+    if(valor === '='){
+        let valor_campo = document.getElementById('resultado').value
+        console.log(valor_campo)
+    }
+       }else if(tipo === 'valor'){
+         document.getElementById('resultado').value += valor
+       }
+}
