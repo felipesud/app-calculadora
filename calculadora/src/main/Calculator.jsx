c112048a9a0d3db942a5a3b770b3b6ee64e119ee
Felipe Belisário <felipesantos.dsb@hotmail.com>
2021-09-19 22:46:38 -0300

    Solving NaN bug

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -13,38 +13,42 @@ const initialState = {
 
 export default class Calculator extends Component {
 
-    state = {...initialState}
+    state = { ...initialState }
 
-constructor(props){
-    super(props)
-    this.clearMemory = this.clearMemory.bind(this)
-    this.setOperation = this.setOperation.bind(this)
-    this.addDigit = this.addDigit.bind(this)
-}
-    clearMemory(){
-        this.setState({...initialState})
+    constructor(props) {
+        super(props)
+        this.clearMemory = this.clearMemory.bind(this)
+        this.setOperation = this.setOperation.bind(this)
+        this.addDigit = this.addDigit.bind(this)
+    }
+    clearMemory() {
+        this.setState({ ...initialState })
     }
 
-    setOperation(operation){
+    setOperation(operation) {
         if (this.state.current === 0) {
-            this.setState({ operation, current: 1, clearDisplay: true})
-        }else {
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } else {
             const equals = operation === '='
             const currentOperation = this.state.operation
 
             const values = [...this.state.values]
             try {
-            values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
-        } catch(e) {
-            values[0] = this.state.values[0]
-        }
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+                if (isNaN(values[0]) || !isFinite(values[0])) {
+                    this.clearMemory()
+                    return
+                }
+            } catch (e) {
+                values[0] = this.state.values[0]
+            }
             values[1] = 0
 
             this.setState({
-                displayValue: values[0], 
-                operation: equals ? null : operation, 
-                current: equals ? 0: 1,
-                clearDisplay: !equals, 
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
                 values
             })
 
@@ -52,17 +56,17 @@ constructor(props){
         }
     }
 
-    addDigit(n){
-        if (n === '.' && this.state.displayValue.includes('.')){
+    addDigit(n) {
+        if (n === '.' && this.state.displayValue.includes('.')) {
             return
         }
         const clearDisplay = this.state.displayValue === '0'
-        || this.state.clearDisplay
+            || this.state.clearDisplay
         const currentValue = clearDisplay ? '' : this.state.displayValue
         const displayValue = currentValue + n
-        this.setState({displayValue, clearDisplay: false})
+        this.setState({ displayValue, clearDisplay: false })
 
-        if(n !== '.'){
+        if (n !== '.') {
             const i = this.state.current
             const newValue = parseFloat(displayValue)
             const values = [...this.state.values]
@@ -71,7 +75,7 @@ constructor(props){
         }
     }
 
-    render(){
+    render() {
         return (
             <div className="calculator">
                 <Display value={this.state.displayValue} />
