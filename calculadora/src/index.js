ba75ab639b179e9ea59dfbb93e7a83c88aa93dd9
Felipe Belisário <felipesantos.dsb@hotmail.com>
2021-09-19 22:16:31 -0300

    Logic Added

--- calculadora/src/index.js
+++ calculadora/src/index.js
@@ -9,7 +9,7 @@ ReactDOM.render(
   <h1> Calculadora </h1>
 <Calculator />
 </div>,
-  document.getElementById('roots')
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
